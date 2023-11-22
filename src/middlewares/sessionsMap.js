b46b8f5f637fa2cf3ac33f7f4e1e06f93f274d68
Josue Paz <132326243+JosuePaz02@users.noreply.github.com>
2023-11-22 00:07:46 -0600

    Arreglando los maps

--- src/middlewares/sessionsMap.js
+++ src/middlewares/sessionsMap.js
@@ -1,22 +1,21 @@
-// Crear un Map para almacenar información de sesiones con movimientos y horarios
 const sessionsMap = new Map();
 
-// Ejemplo de cómo agregar una sesión con movimientos y horarios al mapa después de un inicio de sesión exitoso
-function agregarSesionConMovimientosYHorarios(idSesion, datosSesion) {
-  // Inicializar un array para almacenar los movimientos
+//* Ejemplo de cómo agregar una sesión con movimientos y horarios al mapa después de un inicio de sesión exitoso
+const agregarSesionConMovimientosYHorarios = (idSesion, datosSesion) => {
+  //! Inicializar un array para almacenar los movimientos
   datosSesion.movimientos = [];
-  // Agregar la hora de inicio
+  //! Agregar la hora de inicio
   datosSesion.horaInicio = new Date();
   sessionsMap.set(idSesion, datosSesion);
 }
 
-// Ejemplo de cómo obtener información de una sesión con movimientos y horarios
-function obtenerDatosDeSesionConMovimientosYHorarios(idSesion) {
+//* Ejemplo de cómo obtener información de una sesión con movimientos y horarios
+const obtenerDatosDeSesionConMovimientosYHorarios = (idSesion) => {
   return sessionsMap.get(idSesion);
 }
 
-// Ejemplo de cómo agregar un movimiento a una sesión
-function agregarMovimientoASesion(idSesion, movimiento) {
+//* Ejemplo de cómo agregar un movimiento a una sesión
+const agregarMovimientoASesion = (idSesion, movimiento) => {
   const datosSesion = sessionsMap.get(idSesion);
   if (datosSesion) {
     datosSesion.movimientos.push(movimiento);
@@ -24,21 +23,21 @@ function agregarMovimientoASesion(idSesion, movimiento) {
   }
 }
 
-// Ejemplo de cómo actualizar información de sesión con movimientos y horarios
-function actualizarDatosDeSesionConMovimientosYHorarios(idSesion, nuevosDatos) {
+//* Ejemplo de cómo actualizar información de sesión con movimientos y horarios
+const actualizarDatosDeSesionConMovimientosYHorarios = (idSesion, nuevosDatos) => {
   const datosSesion = sessionsMap.get(idSesion);
   if (datosSesion) {
-    // Actualizar solo si la sesión existe
+    //! Actualizar solo si la sesión existe
     Object.assign(datosSesion, nuevosDatos);
     sessionsMap.set(idSesion, datosSesion);
   }
 }
 
-// Ejemplo de cómo cerrar una sesión con movimientos y horarios (por ejemplo, al cerrar sesión)
-function cerrarSesionConMovimientosYHorarios(idSesion) {
+//* Ejemplo de cómo cerrar una sesión con movimientos y horarios (por ejemplo, al cerrar sesión)
+const cerrarSesionConMovimientosYHorarios = (idSesion) => {
   const datosSesion = sessionsMap.get(idSesion);
   if (datosSesion) {
-    // Agregar la hora de cierre
+    //! Agregar la hora de cierre
     datosSesion.horaCierre = new Date();
     sessionsMap.set(idSesion, datosSesion);
   }
