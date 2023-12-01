0de8fc797bcbb35c23b9452ac034ddf69b50f036
moisesheras <moisesheras@icloud.com>
2023-11-30 19:45:20 -0600

    a

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,10 +1,18 @@
 //moi
 
-vistaprincipal =( req, res)=>{
-    res.render('menu')
+const vistaprincipal =( req, res)=>{
+    res.render('home')
 }
 
+const vistalinks =( req, res)=>{
+    res.render('links')
+}
+const vistanotificaciones =( req, res)=>{
+    res.render('notificaciones')
+}
 
 module.exports ={
-    vistaprincipal
+    vistaprincipal,
+    vistalinks,
+    vistanotificaciones
 }
--- src/routes/links.routes.js
+++ src/routes/links.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const { linksGet } = require('../controllers/index.controller.js')
 /*moi*/
-const {vistaprincipal} = require('../controllers/controller.js')
-/*router.get('/', vistaprincipal)
+const {vistaprincipal, vistalinks, vistanotificaciones} = require('../controllers/controller.js')
+/*router.get('/', vistaprincipal)*/
+/*router.get('/links', vistalinks)
+/*router.get('/notificaciones',vistanotificaciones)
+
 /*module.exports = {routes : links.routes}*/
 
 const router = express.Router()
