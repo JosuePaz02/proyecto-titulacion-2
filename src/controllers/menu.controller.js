a96e2a98c79c977d7a9a8fe5c6df2750091cf4f0
Josue Paz <132326243+JosuePaz02@users.noreply.github.com>
2024-01-08 23:01:37 -0600

    Revert "a"
    This reverts commit 1dc6d1fc85977a20b2650464e2064556c6bcf651.

--- src/controllers/menu.controller.js
+++ src/controllers/menu.controller.js
@@ -19,7 +19,7 @@ const linksGet = async (req, res) => {
       console.log("Usuario no autenticado");
     }
   }, 10000);
-  res.render("layout.ejs");
+  res.render("menu.ejs");
 };
 
 const vistaprincipal = (req, res) => {
