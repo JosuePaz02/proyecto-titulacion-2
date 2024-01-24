08ba4b63e20f4800f70cc21c741b639c53f9510e
moisesheras <moisesheras@icloud.com>
2024-01-23 19:16:32 -0600

    q

--- src/controllers/menu.controller.js
+++ src/controllers/menu.controller.js
@@ -41,9 +41,9 @@ module.exports = {
 
 //aqui voy a poner los scripts para la ventana modal
 
-function btnAbrirModal()  {document.querySelector("#btn-abrir-modal")}
-function  btnCerrarModal()  {document.querySelector("#btn-cerrar-modal")}
-function modal()  {document.querySelector("#modal")}
+//const btnAbrirModal =  document.querySelector("#btn-abrir-modal")
+//const btnCerrarModal =  document.querySelector("#btn-cerrar-modal")
+//const modal =  document.querySelector("#modal")
 
 //btnAbrirModal.addEventListener("click",()=>{
   //modal.showModal();
@@ -51,3 +51,20 @@ function modal()  {document.querySelector("#modal")}
 //btnCerrarModal.addEventListener("click",()=>{
   //modal.closest();
 //})
+
+//function openModal() {
+  //document.getElementById('myModal').style.display = 'flex';
+//}
+
+// Función para cerrar la ventana modal
+//function closeModal() {
+  //document.getElementById('myModal').style.display = 'none';
+//}
+
+// Cerrar la ventana modal haciendo clic fuera de ella
+//window.addEventListener('click', function(event) {
+  //var modal = document.getElementById('myModal');
+  //if (event.target === modal) {
+    //modal.style.display = 'none';
+  //}
+//});
