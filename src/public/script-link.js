29092bf052b08f60a9e5ec64fd88df35f783ff8d
Machoopresor <kevin_mora250@hotmail.com>
2024-01-31 06:05:56 -0600

    interfaz de login
    faltan 2 cuadros

---
+++ src/public/script-link.js
@@ -0,0 +1,77 @@
+//BOTONES PARA MESES SIN INTERESES 
+                  document.addEventListener("DOMContentLoaded", function () {
+                            const montoInput = document.getElementById("monto");
+                    const mesesRadio = document.getElementsByName("meses");
+
+                            montoInput.addEventListener("input", function () {
+                              const monto = parseInt(montoInput.value, 10);
+
+                              // Habilitar opciones de meses según la condición del monto
+                              if (monto > 0 && monto < 300) {
+                                habilitarOpciones([0]); // Agregamos el índice 3 para el pago único
+                              } else if (monto >= 300 && monto < 600) {
+                                habilitarOpciones([0, 1]); // Agregamos el índice 3 para el pago único
+                              } else if (monto >= 600 && monto < 1200) {
+                                habilitarOpciones([0, 1, 2]); // Agregamos el índice 3 para el pago único
+                              } else if (monto >= 1200) {
+                                habilitarOpciones([0, 1, 2, 3]); // Agregamos el índice 3 para el pago único
+                              } else {
+                                deshabilitarOpciones();
+                              }
+                            });
+
+                            function habilitarOpciones(opciones) {
+                              // Habilitar las opciones indicadas en el array
+                              for (let i = 0; i < mesesRadio.length; i++) {
+                                mesesRadio[i].disabled = !opciones.includes(i);
+                                mesesRadio[i].checked = false;
+                              }
+                            }
+
+                            function deshabilitarOpciones() {
+                              // Deshabilitar todas las opciones
+                              for (let i = 0; i < mesesRadio.length; i++) {
+                                mesesRadio[i].disabled = true;
+                                mesesRadio[i].checked = false;
+                              }
+                            }
+
+                            // Event listener para desmarcar radio buttons cuando se hace clic en su label
+                            const labels = document.querySelectorAll(
+                              ".input-container label"
+                            );
+                            labels.forEach((label) => {
+                              label.addEventListener("click", function () {
+                                const radio = this.previousElementSibling;
+                                radio.checked = !radio.checked;
+                              });
+                            });
+                  });
+
+//BOTON DE VENTANA MODAL
+                    // Ventana modal
+                    var modal = document.getElementById("ventanaModal");
+
+                    // Botón que abre el modal
+                    var boton = document.getElementById("abrirModal");
+
+                    // Hace referencia al elemento <span> que tiene la X que cierra la ventana
+                    var span = document.getElementsByClassName("cerrar")[0];
+
+                    // Cuando el usuario hace click en el botón, se abre la ventana
+                    boton.addEventListener("click", function () {
+                      modal.style.display = "block";
+                    });
+
+                    // Si el usuario hace click en la x, la ventana se cierra
+                    span.addEventListener("click", function () {
+                      modal.style.display = "none";
+                    });
+
+                    // Si el usuario hace click fuera de la ventana, se cierra.
+                    window.addEventListener("click", function (event) {
+                      if (event.target == modal) {
+                        modal.style.display = "none";
+                      }
+                    });
+                  
