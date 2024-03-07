6adddaf92f1923b2e196512f308a468420a83bf0
Josue Paz <132326243+JosuePaz02@users.noreply.github.com>
2024-03-07 15:30:06 -0600

    Subiendo xlsx pupulate

--- src/middlewares/generacionXlsx.js
+++ src/middlewares/generacionXlsx.js
@@ -1,6 +1,13 @@
-const xlsx = require('xlsx-populate')
-
+const xlsx = require("xlsx-populate");
 
 const generacionMasiva = async () => {
-    const workbook = await xlsx.fromBlankAsync();
-}
+  const workbook = await xlsx.fromBlankAsync();
+
+  workbook.sheet(0).cell("A1").value("ID");
+  workbook.sheet(0).cell("B1").value("Comercio");
+  workbook.sheet(0).cell("C1").value("Nombre");
+  workbook.sheet(0).cell("D1").value("Email");
+  workbook.sheet(0).cell("F1").value("Tel");
+  workbook.sheet(0).cell("G1").value("Monto");
+  workbook.sheet(0).cell("H1").value("Productos");
+};
